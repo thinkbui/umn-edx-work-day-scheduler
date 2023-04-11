be514515c16d26fd5487fc26c818a0eb2acf9c86
Ian Bui <thinkbui@gmail.com>
2023-04-11 00:14:14 -0500

    Removed todo

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,6 +1,3 @@
-// Wrap all code that interacts with the DOM in a call to jQuery to ensure that
-// the code isn't run until the browser has finished rendering all the elements
-// in the html.
 $(function () {
   datetime_now = dayjs();
   calData = {};
